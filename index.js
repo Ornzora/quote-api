3797e803cdb1d3a9814ed546399878748b8f90ae
Myouka <ornzoraneko@gmail.com>
2026-09-05 09:19:47 +0700

    Keep local Koa server startup in index

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 require('dotenv').config({ path: './.env' })
-require('./app')
+
+const { app, ready } = require('./app')
+const port = process.env.PORT || 3000
+
+ready.then(() => {
+  app.listen(port, () => {
+    console.log('Listening on localhost, port', port)
+  })
+}).catch((error) => {
+  console.error(error)
+  process.exit(1)
+})
